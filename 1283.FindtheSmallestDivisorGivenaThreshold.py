eb91fcc444d5d99ec7ebf26790eb536f0e715a41
Aditya Tuljapurkar <adityatuljapurkar90018@gmail.com>
2025-01-13 23:52:29 +0530

    update 1283.FindtheSmallestDivisorGivenaThreshold.py

--- 1283.FindtheSmallestDivisorGivenaThreshold.py
+++ 1283.FindtheSmallestDivisorGivenaThreshold.py
@@ -2,21 +2,19 @@ import math
 nums = [1,2,5,9]
 threshold = 6
 def function (nums:list[int], threshold:int):
-    s , e = 0, len(nums)-1
-    result = nums[e] 
-    while s< e :
+    s , e = 1, max(nums)
+    while s < e:
         mid = (s+e)//2
         sum = 0
-        for i in nums : 
-            sum += math.ceil(i)/nums[mid]
-        if sum <= threshold : 
-            result = nums[mid] 
-            e = mid-1
-        elif sum > threshold :
-            s = mid+1
-    return result
-            
+        for num in nums:
+            sum += math.ceil(num/mid)
+        if sum <= threshold:
+            e = mid 
+        
+        else : 
+            s = mid +1
+    return s
+
             
     
 print(function (nums,threshold))
-        
