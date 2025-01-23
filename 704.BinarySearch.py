0a1657d1473fa494b374461fd0d295cd8c065ebb
Aditya Tuljapurkar <adityatuljapurkar90018@gmail.com>
2025-01-23 23:01:20 +0530

    add 704.BinarySearch.py

---
+++ 704.BinarySearch.py
@@ -0,0 +1,18 @@
+nums = [-1,0,3,5,9,12] 
+target = 9
+class ABC : 
+    def function (self, nums, target):
+        left = 0
+        right = len(nums) - 1
+        while left <= right : 
+            mid = (left + right) // 2
+            if nums[mid] == target :
+                return mid
+            elif nums[mid]<target :
+                left = mid + 1
+            else :
+                right = mid -1
+        return -1
+
+obj = ABC()
+print(obj.function(nums, target))  # Output: 4
