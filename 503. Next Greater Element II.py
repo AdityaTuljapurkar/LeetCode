16f661a62f58e28399778d24424aef73daee5a8b
Aditya Tuljapurkar <adityatuljapurkar90018@gmail.com>
2025-05-10 05:06:40 +0530

    add 503 greedy

---
+++ 503. Next Greater Element II.py
@@ -0,0 +1,19 @@
+nums = [1,2,3,4,3]
+from typing import List
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        stack = []
+        result = []
+        n = len(nums)
+        for i in range(n*2-1,-1,-1):
+            current = nums[i%n]
+            while stack and stack[-1] <= current :
+                stack.pop()
+            if i< n :
+                result.append(stack[-1]if stack else -1)
+            stack.append(current)
+        return result[::-1]
+    
+sol = Solution()
+print(sol.nextGreaterElements(nums))
+    
